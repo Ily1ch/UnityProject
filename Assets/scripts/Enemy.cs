e31e4156115038008fe9bc2eb7fde255523d49ee
Ily1ch <kudelkoilya@gmail.com>
2023-03-19 16:16:52 +0300

    6
    6

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 
 public class Enemy : MonoBehaviour
 {
-    public Animator anim;
+    public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
     void Start()
@@ -15,18 +15,17 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        animator.SetTrigger("hurt");
         if (currentHealth <= 0)
             Die();
     }
     void Die()
     {
-        anim.StopPlayback();
-        anim.Play("Death");
         Debug.Log("Dead!!!");
+        animator.SetBool("Death", true);
         Destroy(this.gameObject);
         //this.enabled = false;
         //GetComponent<Collider2D>().enabled = false;
         
     }
-
 } 
